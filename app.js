f18f2c2e2c2311396627a80f64ff0f36780bce3c
unknown <osoriomendozawalter>
2022-08-06 18:04:25 -0500

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+
 
 const conexion = require('./routers/Conexion')
 
@@ -19,15 +22,18 @@ setInterval(function () {
 
 // permitir la solicitud externa de las apis
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+// 	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+// 	next();
+// });
 // fin de permitir
 
+app.use(cors({
+	origin: 'https://www.prayside.com'
+}))
 
 app.use('/', require('./routers/Usuarios'));
 
