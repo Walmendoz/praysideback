9b3b2a8a44ee1aad029551072bf531f135991a6d
unknown <osoriomendozawalter>
2022-08-06 11:04:47 -0500

    make it better

--- routers/Usuariosprueba.js
+++ routers/Usuariosprueba.js
@@ -15,6 +15,17 @@ router.get('/', (req, res) => {
         
 })
 
+router.get('/consulta', (req, res) => {
+    conexion.query('SELECT * from prayside_usuarios where nombre = ? and pais = ?' , [req.params.nombre, req.params.pais],  (err, rows, fields) => {
+    if (err) {
+        console.log(err)
+    }else{
+        res.json(rows)
+    }
+    })
+        
+})
+
 router.get('/consulta/:nombre', (req, res) => {
     conexion.query('SELECT * from prayside_usuarios where nombre = ?' , [req.params.nombre], (err, rows, fields) => {
     if (err) {
