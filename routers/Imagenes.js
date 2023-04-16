1631ade7da0c6897910395e93ecaa347cf6bbeca
unknown <osoriomendozawalter>
2023-04-16 08:10:49 -0500

    otro commit 7

--- routers/Imagenes.js
+++ routers/Imagenes.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 //const ftpStorage = require('multer-ftp');
 
 //var multer = require('multer')
-const sftpStorage = require('multer-sftp')
+//const sftpStorage = require('multer-sftp')
 //Uploading Una sola imagen 
 router.post('/cargarimagen', (req, res) => {
    // sftp settings     
