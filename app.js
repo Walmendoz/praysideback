4636ad3b7cd73bb131e61c8f931dc59c2eaa23d0
unknown <osoriomendozawalter>
2023-04-23 11:33:44 -0500

    otro commit 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const body_parser = require("body-parser");
-//const multer = require("multer");
+const multer = require("multer");
 
 const conexion = require("./funciones/Conexion");
 //const Rutas = require("./rutas/Rutas");
@@ -38,11 +38,11 @@ app.use(body_parser.json()); //Content-type aplication
 app.use(body_parser.raw({ type: "image/*", limit: "1mb" }));
 
 // Midleware
-//app.use(
-//  multer({
-//    dest: "./src/public/uploads",
-//  }).single("image")
-//);
+app.use(
+  multer({
+    dest: "./src/public/uploads",
+  }).single("image")
+);
 
 //Apis web
 //app.use("/", Rutas);
