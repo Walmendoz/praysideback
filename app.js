241a20fbed03e44cad5fcf2e664362d2bdc25f40
unknown <osoriomendozawalter>
2024-02-04 17:05:48 -0500

    back commit 9

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ setInterval(function () {
   conexion.query(`SELECT * from prayside_usuarios where email = '1' `);
 }, 5000);
 
-//"https://prayside.com",
-
 const whitelist = [
+  "https://prayside.com",
   "https://google.com",
   "https://praysidefront.app.mendozapps.com",
 ];
